ccbf93a9f2b3a6c3af3aec09cc0092e6e86ef0cb
xiaohaibiao <xiaohaibiao331@outlook.com>
2019-01-18 10:01:40 +0800

    解决购物车同商品同tag合并问题

--- pages/shopcenter/index/index.js
+++ pages/shopcenter/index/index.js
@@ -27,7 +27,7 @@ Page({
     itemSpace: [],
     menuSpace: [],
     activeIndex: 0,
-    activePopIndexList: [0,0,0,0],
+    activePopIndexList: [0, 0, 0, 0],
     itemList: [],
     toView: 'a0',
     leftScrollTop: 0,
@@ -40,11 +40,11 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     var that = this;
     this.setHeight();
     this.getListData();
-    if (options.categoryid>=0){
+    if (options.categoryid >= 0) {
       this.jumpToCategory(options.categoryid);
     }
   },
@@ -53,22 +53,31 @@ Page({
    * 加入购物车
    */
   addToCart: function(e) {
+    var merge = false;
     var goodDic = {};
     var goodsInfo = this.data.goodsInfo;
+    var cost = parseFloat((this.data.cartInfo.cost * 100 + goodsInfo.specfoods[0].price * 100) / 100).toFixed(2);
     var goodsList = this.data.cartInfo.goodsList;
     var itemList = this.data.itemList;
     var tag = itemList.join("+");
 
-    goodDic["tag"] = tag;
-    goodDic["id"] = goodsInfo.item_id;
-    goodDic["name"] = goodsInfo.name;
-    goodDic["price"] = goodsInfo.specfoods[0].price;
-    goodDic["num"] = 1;
+    for(var count=0; goodsList.length > count; count++){
+      if (goodsList[count].id == goodsInfo.item_id && goodsList[count].tag == tag){
+        goodsList[count].num += 1;
+        merge = true;
+        break;
+      }
+    }
 
-    
-    var cost = parseFloat((this.data.cartInfo.cost * 100 + goodDic["price"] * 100) / 100).toFixed(2);
+    if(!merge){
+      goodDic["tag"] = tag;
+      goodDic["id"] = goodsInfo.item_id;
+      goodDic["name"] = goodsInfo.name;
+      goodDic["price"] = goodsInfo.specfoods[0].price;
+      goodDic["num"] = 1;
+      goodsList.push(goodDic);
+    }
 
-    goodsList.push(goodDic);
     this.setData({
       'cartInfo.goodsList': goodsList,
       'cartInfo.cost': cost,
@@ -82,14 +91,14 @@ Page({
   minusNum: function(e) {
     var index = e.currentTarget.dataset.index;
     var goodsList = this.data.cartInfo.goodsList;
-    var cost = parseFloat((this.data.cartInfo.cost*100 - goodsList[index].price*100)/100).toFixed(2);
+    var cost = parseFloat((this.data.cartInfo.cost * 100 - goodsList[index].price * 100) / 100).toFixed(2);
     goodsList[index].num -= 1;
 
-    if(goodsList[index].num == 0){
+    if (goodsList[index].num == 0) {
       goodsList.splice(index, 1);
     }
-    
-    if(goodsList.length==0){
+
+    if (goodsList.length == 0) {
       this.setData({
         showCart: false,
       });
@@ -142,7 +151,7 @@ Page({
     var currentGoods = e.currentTarget.dataset.index;
     var goodsInfo = this.data.listData[currentType].foods[currentGoods];
 
-    goodsInfo.attrs.forEach(function(e){
+    goodsInfo.attrs.forEach(function(e) {
       itemListDef.push(e.values[0]);
     });
 
@@ -158,7 +167,7 @@ Page({
    * 隐藏info弹窗
    */
   tapMask: function() {
-    if(this.data.popUp) {
+    if (this.data.popUp) {
       this.setData({
         popUp: false,
       });
@@ -177,13 +186,13 @@ Page({
   /**
    * 滑动右侧list
    */
-  scrollRightMenu: function (e) {
+  scrollRightMenu: function(e) {
     var that = this;
     var itemSpace = this.data.itemSpace;
     var scrollTop = e.detail.scrollTop;
     var count = 0
-    for(count; itemSpace.length > count; count++){
-      if (scrollTop < itemSpace[count]){
+    for (count; itemSpace.length > count; count++) {
+      if (scrollTop < itemSpace[count]) {
         this.setData({
           activeIndex: count,
         });
@@ -192,7 +201,7 @@ Page({
     }
 
     /**保持左scroll-view焦点在当前类别 */
-    that.switchBanner(scrollTop).then(()=> {
+    that.switchBanner(scrollTop).then(() => {
       var menuTag = that.data.menuSpace[count];
       var leftScrollTop = that.data.leftScrollTop;
       var listHeight = that.data.listHeight;
@@ -200,7 +209,7 @@ Page({
         that.setData({
           leftScrollTop: menuTag - listHeight,
         });
-      } else if (menuTag <= leftScrollTop + that.data.menuSpace[0]){
+      } else if (menuTag <= leftScrollTop + that.data.menuSpace[0]) {
         that.setData({
           leftScrollTop: menuTag - that.data.menuSpace[0],
         });
@@ -214,10 +223,10 @@ Page({
    */
   selectMenu: function(e) {
     var index = e.currentTarget.dataset.index;
-    if(index>=0){
+    if (index >= 0) {
       this.setData({
         activeIndex: index,
-        toView: 'a'+index,
+        toView: 'a' + index,
       });
     }
     //console.log(e.currentTarget.dataset.index)
@@ -226,7 +235,7 @@ Page({
   /**
    * 点击购物车
    */
-  showCartList: function () {
+  showCartList: function() {
     if (this.data.cartInfo.goodsList.length != 0) {
       this.setData({
         'showCart': !this.data.showCart,
@@ -237,7 +246,7 @@ Page({
   /**
    * 清空购物车
    */
-  clearCartList: function () {
+  clearCartList: function() {
     console.log("clear cartlist success")
     this.setData({
       'cartInfo.goodsList': [],
@@ -250,9 +259,9 @@ Page({
   /**
    * 切换banner
    */
-  switchBanner: function (scrollTop) {
+  switchBanner: function(scrollTop) {
     var that = this;
-    return new Promise(function(resolve, reject){
+    return new Promise(function(resolve, reject) {
       if (scrollTop > 300) {
         that.setData({
           'banner.showBanner': false
@@ -270,16 +279,16 @@ Page({
   /**
    * 跳转至对应类别
    */
-  jumpToCategory: function (categoryid) {
+  jumpToCategory: function(categoryid) {
     this.setData({
       activeIndex: categoryid,
     });
   },
 
   /**
- * 设置高度
- */
-  setHeight: function () {
+   * 设置高度
+   */
+  setHeight: function() {
     this.caclHeight('.height').then((res) => {
       this.setData({
         listHeight: wx.getSystemInfoSync().windowHeight - res[0] - res[1],
@@ -291,11 +300,11 @@ Page({
   /**
    * 计算高度
    */
-  caclHeight: function (clzz) {
-    return new Promise(function (resolve, reject) {
+  caclHeight: function(clzz) {
+    return new Promise(function(resolve, reject) {
       var height = [];
-      wx.createSelectorQuery().selectAll(clzz).boundingClientRect(function (rects) {
-        rects.forEach(function (rect) {
+      wx.createSelectorQuery().selectAll(clzz).boundingClientRect(function(rects) {
+        rects.forEach(function(rect) {
           height.push(rect.height);
         })
         resolve(height)
@@ -311,7 +320,7 @@ Page({
     that.caclHeight('.listCellHeight').then((res) => {
       var total = 0;
       var item = [];
-      res.forEach(function (e) {
+      res.forEach(function(e) {
         item.push(total += e);
       });
       that.setData({
@@ -319,10 +328,10 @@ Page({
       });
     });
 
-    that.caclHeight('.listMenuHeight').then((res)=>{
+    that.caclHeight('.listMenuHeight').then((res) => {
       var total = 0;
       var item = [];
-      res.forEach(function (e) {
+      res.forEach(function(e) {
         item.push(total += e);
       });
       that.setData({
@@ -336,30 +345,29 @@ Page({
    */
   getListData: function() {
     var that = this;
-    util.request(api.GetList,
-      {}, "GET").then((res) => {
-        res.forEach(function (e) {
-          e['num'] = 0;
-        });
-        that.setData({
-          listData: res,
-          'loading.loadingShow': false,
-          'loading.loadingError': false,
-        });
-        that.getSpace();
-      }).catch((err) => {
-        console.log('load failed');
-        that.setData({
-          'loading.loadingShow': false,
-          'loading.loadingError': true,
-        });
+    util.request(api.GetList, {}, "GET").then((res) => {
+      res.forEach(function(e) {
+        e['num'] = 0;
       });
+      that.setData({
+        listData: res,
+        'loading.loadingShow': false,
+        'loading.loadingError': false,
+      });
+      that.getSpace();
+    }).catch((err) => {
+      console.log('load failed');
+      that.setData({
+        'loading.loadingShow': false,
+        'loading.loadingError': true,
+      });
+    });
   },
 
   /**
    * 点击付款
    */
-  pay: function () {
+  pay: function() {
     if (this.data.cartInfo.goodsList.length != 0) {
       wx.setStorageSync("cartInfo", this.data.cartInfo)
       wx.navigateTo({
@@ -367,9 +375,9 @@ Page({
       })
     }
   },
-  
+
   /**
    * scroll-view 滚动层穿透问题
    */
-  move: function(){},
+  move: function() {},
 })
